285785a8fb178817109833b3af82489cc5f5cb1e
Mridul2820 <immridulpanda2820@gmail.com>
2021-12-21 19:08:23 +0530

    Router Setup

--- shareme-frontend/src/App.js
+++ shareme-frontend/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import { Routes, Route, useNavigate } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
 
 const App = () => {
     return (
-        <div>
-            ShareMe
-        </div>
+        <Routes>
+            <Route 
+                path="login"
+                element={<Login />}
+            />
+            <Route 
+                path="/"
+                element={<Home />}
+            />
+        </Routes>
     )
 }
 
