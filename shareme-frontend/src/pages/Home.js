8c741da7a4afeb600e0c72774b3b18b6c711ced1
Mridul2820 <immridulpanda2820@gmail.com>
2021-12-22 00:38:20 +0530

    mobile screen sidebar create

--- shareme-frontend/src/pages/Home.js
+++ shareme-frontend/src/pages/Home.js
@@ -1,9 +1,101 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import { HiMenu } from 'react-icons/hi'
+import { AiFillCloseCircle } from 'react-icons/ai'
+import { Link, Route, Routes } from 'react-router-dom'
+
+// Components
+import Sidebar from '../components/Sidebar'
+import UserProfile from './UserProfile'
+import Pins from './Pins'
+
+import { client } from '../client'
+import { userQuery } from '../utils/data'
+import ShareMeLogo from '../assets/logo.png'
 
 const Home = () => {
+    const [toggleSidebar, setToggleSidebar] = useState(false)
+    const [user, setUser] = useState()
+    const scrollRef = useRef(null);
+
+    const userInfo = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+
+    useEffect(() => {
+        const query = userQuery(userInfo?.googleId)
+
+        client.fetch(query).then((data) => {
+            setUser(data[0]);
+        });
+    }, [])
+
+    useEffect(() => {
+        scrollRef.current.scrollTo(0, 0);
+      });
+
+    console.log(user)
+
     return (
-        <div>
-            home
+        <div className="flex bg-gray-50 md:flex-row flex-col h-screen transition-height duration-75 ease-out">
+            <div className="hidden md:flex h-screen flex-initial">
+                <Sidebar user={user && user} />
+            </div>
+            <div className="flex md:hidden">
+                <div className="p-2 w-full flex flex-row justify-between items-center shadow-md">
+                    <HiMenu 
+                        fontSize={40}
+                        className="cursor-pointer"
+                        onClick={() => setToggleSidebar(true)}
+                    />
+                    <Link to="/">
+                        <img 
+                            src={ShareMeLogo} 
+                            alt="logo"
+                            className="w-28" 
+                        />
+                    </Link>
+                    <Link to={`user/${user?.id}`}>
+                        {user?.image ? 
+                            <img 
+                                src={user?.image} 
+                                alt="logo"
+                                className="w-9 h-9 rounded-full"
+                            />
+                            :
+                            <div 
+                                className="w-9 h-9 rounded-full bg-slate-300"
+                            />
+                        }
+                    </Link>
+                </div>
+                {toggleSidebar && (
+                    <div className="fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in">
+                        <div className="absolute w-full flex justify-end items-center p-2">
+                            <AiFillCloseCircle 
+                                fontSize={30}
+                                className="cursor-pointer"
+                                onClick={() => setToggleSidebar(false)}
+                            />
+                        </div>
+                        <Sidebar 
+                            user={user && user} 
+                            setToggleSidebar={setToggleSidebar}
+                        />
+                    </div>
+                )}
+            </div>
+            <div className="pb-2 flex-1 h-screen overflow-y-scroll" ref={scrollRef}>
+            <Routes>
+                <Route 
+                    path="/user/:userId" 
+                    element={<UserProfile />} 
+                />
+                <Route 
+                    path="/*" 
+                    element={<Pins user={user && user} />} 
+                />
+            </Routes>
+            </div>
+            <UserProfile />
+            <Pins />
         </div>
     )
 }
