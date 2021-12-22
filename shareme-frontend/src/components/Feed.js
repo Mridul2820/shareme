b6f23c5c60a74dbc5abf97a0fdb100c24e832952
Mridul2820 <immridulpanda2820@gmail.com>
2021-12-22 18:56:17 +0530

    feed and category results show

--- shareme-frontend/src/components/Feed.js
+++ shareme-frontend/src/components/Feed.js
@@ -5,15 +5,36 @@ import MasonryLayout from './MasonryLayout';
 import Spinner from './Spinner';
 
 import { client } from '../client'
+import { feedQuery, searchQuery } from '../utils/data';
 
 const Feed = () => {
-    const [loading, setLoading] = useState(true)
+    const [pins, setPins] = useState()
+    const [loading, setLoading] = useState(false)
+    const { categoryId } = useParams()
+    
+    useEffect(() => {
+        if(categoryId){
+            setLoading(true)
+            const query = searchQuery(categoryId)
+            client.fetch(query).then((data) => {
+                setPins(data);
+                setLoading(false)
+            });
+        }
+        else {
+            setLoading(true)
+            client.fetch(feedQuery).then((data) => {
+                setPins(data);
+                setLoading(false)
+            });
+        }
+    }, [categoryId])
 
     if(loading) return <Spinner message="We are adding new ideas to your feed!" />
 
     return (
         <div>
-            Feed
+            {pins && <MasonryLayout pins={pins} />}
         </div>
     )
 }
--- shareme-frontend/src/pages/Home.js
+++ shareme-frontend/src/pages/Home.js
@@ -32,7 +32,7 @@ const Home = () => {
         scrollRef.current.scrollTo(0, 0);
       });
 
-    console.log(user)
+    // console.log(user)
 
     return (
         <div className="flex bg-gray-50 md:flex-row flex-col h-screen transition-height duration-75 ease-out">
